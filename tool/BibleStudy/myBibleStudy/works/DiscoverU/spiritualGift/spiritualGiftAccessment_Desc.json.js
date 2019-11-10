ac1cfe4371960a29a3db338bcd7577b6ecb1eecc
weiding <weiding@weiding15s-mbp.attlocal.net>
2019-11-10 15:52:14 -0500

    discoverU work ok

---
+++ tool/BibleStudy/myBibleStudy/works/DiscoverU/spiritualGift/spiritualGiftAccessment_Desc.json.js
@@ -0,0 +1,268 @@
+var spiritualGiftAccessment_Desc =
+[
+    "I like to organize people, tasks, and events.",
+
+    "I would like to start churches in places where they do not presently exist.",
+    
+    "I enjoy working creatively with wood, cloth, paints, metal, glass, or other materials.",
+    
+    "I enjoy challenging people's perspective of God by using various forms of art.",
+    
+    "I can readily distinguish between spiritual truth and error, good and evil.",
+    
+    "I tend to see the potential in people.",
+    
+    "I communicate the gospel to others with clarity and effectiveness.",
+    
+    "I find it natural and easy to trust God to answer my prayers.",
+    
+    "I give liberally and joyfully to people in financial need or to projects requiring support.",
+    
+    "I enjoy working behind the scenes to support the work of others.",
+    
+    "I view my home as a place to minister to people in need.",
+    
+    "I take prayer requests from others and consistently pray for them.",
+    
+    "I am approached by people who want to know my perspective on a particular passage or biblical truth.",
+    
+    "I am able to motivate others to accomplish a goal.",
+    
+    "I empathize with hurting people and desire to help in their healing process.",
+    
+    "I can speak in a way that results in conviction and change in the lives of others.",
+    
+    "I enjoy spending time nurturing and caring for others.",
+    
+    "I am able to communicate God's word effectively.",
+    
+    "I am often sought out by others for advice about spiritual or personal matters.",
+    
+    "I am careful, thorough, and skilled at managing details.",
+    
+    "I am attracted to the idea of serving in another country or ethnic community.",
+    
+    "I am skilled in working with different kinds of tools.",
+    
+    "I enjoy developing and using my artistic skills (art, drama, music, photography, etc...).",
+    
+    "I frequently am able to judge a person's character based upon first impressions.",
+    
+    "I enjoy reassuring and strengthening those who are discouraged.",
+    
+    "I consistently look for opportunities to build relationships with non-Christians.",
+    
+    "I have confidence in God's continuing provision and help, even in difficult times.",
+    
+    "I give more than a tithe so that kingdom work can be accomplished.",
+    
+    "I enjoy doing routine tasks that support the ministry.",
+    
+    "I enjoy meeting new people and helping them to feel welcomed.",
+    
+    "I enjoy praying for long periods of time and receive leadings as to what God wants me to pray for.",
+    
+    "I receive information from the Spirit that I did not acquire through natural means.",
+    
+    "I am able to influence others to achieve a vision.",
+    
+    "I can patiently support those going through painful experiences as they try to stabilize their lives.",
+    
+    "I feel responsible to confront others with the truth.",
+    
+    "I have compassion for wandering believers and want to protect them.",
+    
+    "I can spend time in study knowing that presenting truth will make a difference in the lives of people.",
+    
+    "I can often find simple, practical solutions in the midst of conflict or confusion.",
+    
+    "I can clarify goals and develop strategies or plans to accomplish them.",
+    
+    "I am willing to take an active part in starting a new church.",
+    
+    "I enjoy making things for use in ministry.",
+    
+    "I help people understand themselves, their relationships and God better through artistic expression.",
+    
+    "I can see through phoniness or deceit before it is evident to others.",
+    
+    "I give hope to others by directing them to the promises of God.",
+    
+    "I am effective at adapting the gospel message so that it connects with an individual's felt needs.",
+    
+    "I believe that God will help me to accomplish great things.",
+    
+    "I manage my money well in order to free more of it for giving.",
+    
+    "I willingly take on a variety of odd jobs around the church to meet the needs of others.",
+    
+    "I genuinely believe the Lord directs strangers to me who need to get connected to others.",
+    
+    "I am conscious of ministering to others as I pray.",
+    
+    "I am committed, and schedule blocks of time for reading and studying Scripture, to understand Biblical truth fully and accurately.",
+    
+    "I can adjust my leadership style to bring out the best in others.",
+    
+    "I enjoy helping people sometimes regarded as undeserving or beyond help.",
+    
+    "I boldly expose cultural trends, teachings, or events, which contradict Biblical principles.",
+    
+    "I like to provide guidance for the whole person - relationally, emotionally, spiritually, etc...",
+    
+    "I can devote considerable time to learning new Biblical truths in order to communicate them to others.",
+    
+    "I can easily select the most effective course of action from among several alternatives.",
+    
+    "I can identify and effectively use the resources needed to accomplish tasks.",
+    
+    "I can adapt well to different cultures and surroundings.",
+    
+    "I can visualize how something should be constructed before I build it.",
+    
+    "I like finding new and fresh ways of communicating God's truth.",
+    
+    "I tend to see rightness or wrongness in situations.",
+    
+    "I reassure those who need to take courageous action in their faith, family, or life.",
+    
+    "I invite unbelievers to accept Christ as their Savior.",
+    
+    "I trust God in circumstances where success cannot be guaranteed by human effort alone.",
+    
+    "I am challenged to limit my lifestyle in order to give away higher percentages of my income.",
+    
+    "I see spiritual significance in doing practical tasks.",
+    
+    "I like to create a place where people do not feel that they are alone.",
+    
+    "I pray with confidence because I know that God works in response to prayer.",
+    
+    "I have insight or just know something to be true.",
+    
+    "I set goals and manage people and resources effectively to accomplish them.",
+    
+    "I have great compassion for hurting people.",
+    
+    "I see most actions as right or wrong, and feel the need to correct the wrong.",
+    
+    "I can faithfully provide long-term support and concern for others.",
+    
+    "I like to take a systematic approach to my study of the Bible.",
+    
+    "I can anticipate the likely consequences of an individual's or a group's action.",
+    
+    "I like to help organizations or groups become more efficient.",
+    
+    "I can relate to others in culturally sensitive ways.",
+    
+    "I honor God with my handcrafted gifts.",
+    
+    "I apply various artistic expressions to communicate God's truth.",
+    
+    "I receive affirmation from others concerning the reliability of my insights or perceptions.",
+    
+    "I strengthen those who are wavering in their faith.",
+    
+    "I openly tell people that I am a Christian and want them to ask me about my faith.",
+    
+    "I am convinced of God's daily Presence and action in my life.",
+    
+    "I like knowing that my financial support makes a real difference in the lives and ministries of God's people.",
+    
+    "I like to find small things that need to be done and often do them without being asked.",
+    
+    "I enjoy entertaining people and opening my home to others.",
+    
+    "When I hear about needy situations, I feel burdened to pray.",
+    
+    "I have suddenly known some things about others, but did not know how I knew them.",
+    
+    "I influence others to perform to the best of their capability.",
+    
+    "I can look beyond a person's handicaps or problems to see a life that matters to God.",
+    
+    "I like people who are honest and will speak the truth.",
+    
+    "I enjoy giving guidance and practical support to a small group of people.",
+    
+    "I can communicate Scripture in ways that motivate others to study and want to learn more.",
+    
+    "I give practical advice to help others through complicated situations.",
+    
+    "I enjoy learning about how organizations function.",
+    
+    "I enjoy pioneering new undertakings.",
+    
+    "I am good at and enjoy working with my hands.",
+    
+    "I am creative and imaginative.",
+    
+    "I can identify preaching, teaching, or communication which is not true to the Bible.",
+    
+    "I like motivating others to take steps for spiritual growth.",
+    
+    "I openly and confidently tell others what Christ has done for me.",
+    
+    "I am regularly challenging others to trust God.",
+    
+    "I give generously due to my commitment to stewardship.",
+    
+    "I feel comfortable being a helper, assisting others to do their job more effectively.",
+    
+    "I do whatever I can to make people feel that they belong.",
+    
+    "I am honored when someone asks me to pray for them.",
+    
+    "I discover important Biblical truths when reading or studying Scripture which benefit others in the Body of Christ.",
+    
+    "I am able to cast a vision that others want to be a part of.",
+    
+    "I enjoy bringing hope and joy to people living in difficult circumstances.",
+    
+    "I will speak God's truth, even in places where it is unpopular or difficult for others to accept.",
+    
+    "I can gently restore wandering believers to faith and fellowship.",
+    
+    "I can present information and skills to others at a level that makes it easy for them to grasp and apply to their lives.",
+    
+    "I can apply Scriptural truth that others regard as practical and helpful.",
+    
+    "I can visualize a coming event, anticipate potential problems, and develop backup plans.",
+    
+    "I am able to orchestrate or oversee several church ministries.",
+    
+    "I am able to design and construct things that help the church.",
+    
+    "I regularly need to get alone to reflect and develop my imagination.",
+    
+    "I can sense when demonic forces are at work in a person or situation.",
+    
+    "I am able to challenge or rebuke others in order to foster spiritual growth.",
+    
+    "I seek opportunities to talk about spiritual matters with unbelievers.",
+    
+    "I can move forward in spite of position or lack of support when I sense God's blessing on an undertaking.",
+    
+    "I believe I have been given an abundance of resources so that I may give more to the Lord's work.",
+    
+    "I readily and happily use my natural or learned skills to help wherever needed.",
+    
+    "I can make people feel at ease even in unfamiliar surroundings.",
+    
+    "I often see specific results in direct response to my prayers.",
+    
+    "I confidently share my knowledge and insights with others.",
+    
+    "I figure out where we need to go and help others to get there.",
+    
+    "I enjoy doing practical things for others who are in need.",
+    
+    "I feel compelled to expose sin wherever I see it and to challenge people to repentance.",
+    
+    "I enjoy patiently but firmly nurturing others in their development as believers.",
+    
+    "I enjoy explaining things to people so that they can grow spiritually and personally.",
+    
+    "I have insights into how to solve problems that others do not see.",
+]
---
+++ tool/BibleStudy/myBibleStudy/works/DiscoverU/spiritualGift/spiritualGiftAccessment_Key.json.js
@@ -0,0 +1,22 @@
+var spiritualGiftAccessment_Key =
+{
+"A":"Administration", 
+"B":"Apostleship (i.e. Missions)",
+"C":"Craftsmanship",  
+"D":"Creative Communication",
+"E":"Discernment",     
+"F":"Encouragement",
+"G":"Evangelism",       
+"H":"Faith",
+"I":"Giving",
+"J":"Helps", 
+"K":"Hospitality",       
+"L":"Intercession",     
+"M":"Knowledge",      
+"N":"Leadership",      
+"O":"Mercy",              
+"P":"Prophecy",         
+"Q":"Shepherding",   
+"R":"Teaching",          
+"S":"Wisdom"           
+}
