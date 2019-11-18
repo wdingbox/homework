f56464adef090a442954b588a0a0713528ebafd3
weiding <weiding@weiding15s-mbp.attlocal.net>
2019-11-18 11:09:48 -0500

    evangelism assessment

---
+++ tool/BibleStudy/myBibleStudy/works/DiscoverU/evangelism/abilities.js
@@ -0,0 +1,81 @@
+Evangelism Styles Questionnaire
+Scoring Totals:
+3-Very much 2-Somewhat 1-Very Little 0Q-Not at all (Based on your overall tendencies/preferences)
+
+___ 1. In conversations, I like to approach topics directly, without much small talk or beating around the bush.
+
+__ 2. Thave a hard time getting out of bookstores or libraries without getting a bunch of books that will help me better
+understand issues being debated in society.
+
+__ 3. T often tell stories about my personal experiences in order to illustrate a point I am trying to make.
+
+__ 4. Tama "people person" who places a high value on friendship.
+
+__ 5. T enjoy including or adding new people to activities I am involved in.
+
+__ 6. I see needs in people's lives that others often overlook.
+
+__ 7. Ido not shy away from putting a person on the spot when it seems necessary.
+
+__ 8. I tend to be analytical.
+
+__ 9. Toften identify with others by using phrases like "I used to think that too" or "I once felt the way you do.”
+
+__10. Other people have commented about my ability for developing new friendships.
+
+__11. To be honest, even if I knew the answers, I am more comfortable having someone "better qualified” explain
+Christianity to my friends.
+
+__12.1 find fulfillment in helping others often in behind-the-scenes ways.
+
+__13. Ido not have a problem confronting my friends with the truth even if it risks hurting the relationship.
+
+__14. In conversations, I naturally focus on the questions that are holding up a person's spiritual progress.
+
+__15. When I tell people of how I came to Christ, I have found that they have been interested in hearing it.
+
+__16. I would rather delve into personal life issues than abstract theological ideas.
+
+__17. If I knew of a high quality outreach event that my friends would enjoy, I would make a big effort to bring them.
+
+__18. I prefer to show love through my actions more than my words.
+
+__19. I believe that real love often means telling someone the truth, even when it hurts.
+
+__20. I enjoy discussions and debates on difficult questions.
+
+__21. T intentionally share my mistakes with others when it will help them relate to the solutions I have found.
+
+__22. I prefer getting involved in discussions concerning a person's life before dealing with the details of their beliefs.
+
+__23. I tend to watch for spiritually strategic events to bring people to (such as Christian concerts, outreach events, seeker
+services).
+
+__2A, When people are spiritually closed, I have found that my quiet demonstrations of Christian love sometimes make
+them more receptive.
+
+__25. A motto that would fit me is: "Make a difference or a mess, but do something.”
+
+__26. I often get frustrated with people when they use weak arguments or poor logic.
+
+__27. People seem interested in hearing stories about things that have happened in my life.
+
+__28. T enjoy long talks with friends.
+
+__29. Tam always looking for a match between the needs and interests of my friends and the various events, books, etc.,
+that they would enjoy or benefit from.
+
+__30. I feel more comfortable physically assisting a person in the name of Christ than getting involved in religious
+discussions.
+
+__31. I sometimes get in trouble for lacking gentleness and sensitivity in the way I interact with others.
+
+__32. I like to get at the underlying reasons for opinions that people hold.
+
+__33. Iam still amazed at how God brought me to faith in Him and I am motivated to tell people about it.
+
+__34. People generally consider me to be an interactive, sensitive, and caring kind of person.
+
+__35. A highlight of my week would be to take a guest with me to an appropriate church event.
+
+__36. I tend to be more practical and action-oriented than philosophical and idea-oriented.
---
+++ tool/BibleStudy/myBibleStudy/works/DiscoverU/evangelism/evangelism_assessment_key.js
@@ -0,0 +1,9 @@
+var evangelism_assessment_key =
+{
+"A":"Confrontational", 
+"B":"Intellectual",
+"C":"Testimonial",  
+"D":"Interpersonal",
+"E":"Invitational",     
+"F":"Serving"      
+}
